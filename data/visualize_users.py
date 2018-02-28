0d5ef8440ae8f475ccd1d83a4381d08ed60fbff7
urielmandujano <uriel.mandujano14@gmail.com>
2018-02-27 22:25:11 -0600

    Updated visualizations

--- data/visualize_users.py
+++ data/visualize_users.py
@@ -5,8 +5,8 @@ Provides at a glance information of the user dataset
 import matplotlib.pyplot as plt
 from collections import Counter
 
-locations = Counter()
-ages = Counter()
+DEFAULT_DATA_DIR = "data/"
+RAW_DATA = "BX-Users-Cleansed.csv"
 
 def plot_locations(counter):
     counts, locs = zip(*sorted(zip(counter.values(), counter.keys())))
@@ -22,19 +22,24 @@ def plot_ages(counter):
     plt.scatter(ages, counts)
     plt.show()
 
-with open("BX-Users-Cleansed.csv", "r") as userfile:
-    _ = userfile.readline()
-    for line in userfile:
-        line.encode('utf8')
+def main():
+    locations, ages = Counter(), Counter()
+    with open(RAW_DATA, "r") as userfile:
+        _ = userfile.readline()
+        for line in userfile:
+            line.encode('utf8')
 
-        line = line.strip().split(";")[1:]
-        if len(line) < 2 or line[-1] == "NULL":
-            continue
+            line = line.strip().split(";")[1:]
+            if len(line) < 2 or line[-1] == "NULL":
+                continue
 
-        age = int(line[-1].strip("\""))
-        country = line[0].strip("\" ").split(",")[-1]
-        locations[country] += 1
-        ages[age] += 1
+            age = int(line[-1].strip("\""))
+            country = line[0].strip("\" ").split(",")[-1]
+            locations[country] += 1
+            ages[age] += 1
 
-plot_locations(locations)
-plot_ages(ages)
+    plot_locations(locations)
+    plot_ages(ages)
+
+if __name__ == '__main__':
+    main()
