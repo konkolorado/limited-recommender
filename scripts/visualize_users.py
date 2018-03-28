81eb0321d94b93f1db06662a4c90b8f581b26c51
konkolorado <uriel.mandujano14@gmail.com>
2018-03-27 20:59:16 -0500

    Refactor visualize_useres to leverage csv module

--- scripts/visualize_users.py
+++ scripts/visualize_users.py
@@ -3,6 +3,7 @@ Provides at a glance information of the user dataset
 """
 
 import os
+import csv
 from collections import Counter
 
 import plot_graph
@@ -12,22 +13,23 @@ data_directory = os.path.abspath("../data") + os.sep
 source_data = data_directory + "BX-Users-Cleansed.csv"
 TOKEN = "unk"
 
+def pull_country_from_location(location):
+    return location.split(",")[-1]
+
 def main():
     file_checks.assert_location_exists(data_directory)
     file_checks.assert_file_exists(source_data)
 
     locations, ages = Counter(), Counter()
-    with open(source_data, "r") as userfile:
-        _ = userfile.readline()
-        for line in userfile:
-            line = line.strip().split(";")[1:]
-            try:
-                age = int(line[-1].strip("\""))
-            except ValueError:
-                continue
-            country = line[0].strip("\" ").split(",")[-1]
+    with open(source_data, "r") as src:
+        reader = csv.DictReader(src, delimiter=';')
+
+        for row in reader:
+            country = pull_country_from_location(row["Location"])
+
             locations[country] += 1
-            ages[age] += 1
+            if row["Age"] != TOKEN:
+                ages[int(row["Age"])] += 1
 
     plot_graph.plot_top_n(locations, "Nations with most users", "Counts")
     plot_graph.plot_top_n(ages, "User age distribution", "Age [Years]", len(ages))
