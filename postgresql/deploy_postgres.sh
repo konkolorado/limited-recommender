0bcd60b907a17ff6859ea8ff67b89c6859a7dd63
konkolorado <uriel.mandujano14@gmail.com>
2018-04-18 22:50:08 -0500

    Making use of environment variables in image/deployment scripts

--- postgresql/deploy_postgres.sh
+++ postgresql/deploy_postgres.sh
@@ -1,1 +1,9 @@
-docker run -d --name postgres --mount type=bind,src=$(pwd)/../data/,dst=/data postgres
+PGLOCAL=/tmp/pgdata
+PGDATA=/var/lib/postgresql/data
+mkdir -p $PGLOCAL
+
+docker run -d \
+    --name postgres \
+    --mount type=bind,src=$(pwd)/../data/,dst=/data,readonly \
+    --mount type=bind,src=$PGLOCAL,dst=$PGDATA\
+    postgres
