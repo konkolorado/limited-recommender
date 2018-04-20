15d95a708240597386f6405060957c53e9a4d643
konkolorado <uriel.mandujano14@gmail.com>
2018-04-19 21:50:58 -0500

    Added pg initialization tests

--- postgresql/deploy_postgres.sh
+++ postgresql/deploy_postgres.sh
@@ -3,7 +3,7 @@ PGDATA=/var/lib/postgresql/data
 mkdir -p $PGLOCAL
 
 docker run -d \
-    --name postgres \
+    --name postgres -p 5432:5432 \
     --mount type=bind,src=$(pwd)/../data/,dst=/data,readonly \
     --mount type=bind,src=$PGLOCAL,dst=$PGDATA\
     postgres
