8ffc41995453dcc9198ff6cf7bc37f2c28d3009a
konkolorado <uriel.mandujano14@gmail.com>
2018-04-24 21:56:33 -0500

    Integrate web with postgres

--- docker/recommender_web/start_web.sh
+++ docker/recommender_web/start_web.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Apply any pending migartions
+python manage.py migrate --no-input
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn recommender_web.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
+
+python manage.py createsuperuser
