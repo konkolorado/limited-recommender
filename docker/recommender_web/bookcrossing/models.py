053b4233dee461b8b7f09ce3cbc34e86e71cb712
konkolorado <uriel.mandujano14@gmail.com>
2018-04-29 23:00:25 -0500

    Add model str method and Meta class

--- docker/recommender_web/bookcrossing/models.py
+++ docker/recommender_web/bookcrossing/models.py
@@ -10,12 +10,30 @@ class Book(models.Model):
     image_url_m = models.URLField()
     image_url_l = models.URLField()
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.publication_yr}"
+
+    class Meta:
+        ordering = ["title"]
+
 class User(models.Model):
     user_id = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return f"User: {self.user_id} Location:{self.location} Age:{self.age}"
+
+    class Meta:
+        ordering = ["user_id"]
+
 class Rating(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.PROTECT)
     isbn = models.ForeignKey(Book, on_delete=models.PROTECT)
     rating = models.SmallIntegerField()
+
+    def __str__(self):
+        return f"Book {self.isbn}\tUser {self.user_id}: {self.rating}"
+
+    class Meta:
+        ordering = ["isbn"]
