7ccfdcf91f72e424e62ad18084953b8e9a6d4608
konkolorado <uriel.mandujano14@gmail.com>
2018-05-10 22:11:23 -0500

    Updated startup script to collectstatic files (for the API) and to run tests (will remove this command once django specifics are set up)

--- docker/recommender_web/start_web.sh
+++ docker/recommender_web/start_web.sh
@@ -4,9 +4,16 @@
 python manage.py makemigrations
 python manage.py migrate --no-input
 
+# Collect static files
+python manage.py collectstatic --no-input
+
 # Preload database
 python manage.py loadcsv -users_csv $USERS \
   -books_csv $BOOKS -ratings_csv $RATINGS
+
+# Run tests
+#TODO remove when finished
+python manage.py test
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
