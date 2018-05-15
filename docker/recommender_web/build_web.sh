086ba3a8fa71298b95feab643271842037bb287d
konkolorado <uriel.mandujano14@gmail.com>
2018-05-14 22:09:28 -0500

    renames the built web image

--- docker/recommender_web/build_web.sh
+++ docker/recommender_web/build_web.sh
@@ -1,7 +1,7 @@
 pip3 freeze > requirements.txt
 
-docker build -f dockerfile.web -t recommender_web:current .
+docker build -f dockerfile.web -t web:current .
 
-docker rmi recommender_web:latest
-docker tag recommender_web:current recommender_web:latest
-docker rmi recommender_web:current
+docker rmi web:latest
+docker tag web:current web:latest
+docker rmi web:current
