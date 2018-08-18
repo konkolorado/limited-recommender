768fbd4c6e42456620349f25b560104007e4deed
konkolorado <uriel.mandujano14@gmail.com>
2018-08-18 16:33:30 -0500

    Added filter fields for Books and Users

--- docker/recommender_web/api/filters.py
+++ docker/recommender_web/api/filters.py
@@ -24,16 +24,32 @@ class RatingFilterSet(FilterSet):
 class UserFilterSet(FilterSet):
     user_id = django_filters.CharFilter(field_name='user_id',
                                         label="User ID")
+    location = django_filters.CharFilter(field_name='location',
+                                         label="Location",
+                                         lookup_expr='icontains')
+    age = django_filters.NumberFilter(field_name='age',
+                                      label="Age")
 
     class Meta:
         model = User
-        fields = ['user_id']
+        fields = ['user_id', 'location', 'age']
 
 
 class BookFilterSet(FilterSet):
     isbn = django_filters.CharFilter(field_name='isbn',
                                      label="ISBN")
+    title = django_filters.CharFilter(field_name='title',
+                                      label="Title",
+                                      lookup_expr='icontains')
+    author = django_filters.CharFilter(field_name='author',
+                                       label="Author",
+                                       lookup_expr='icontains')
+    publication_yr = django_filters.CharFilter(field_name='publication_yr',
+                                               label="Year of Publication")
+    publisher = django_filters.CharFilter(field_name='publisher',
+                                          label="Publisher",
+                                          lookup_expr='icontains')
 
     class Meta:
         model = Book
-        fields = ['isbn']
+        fields = ['isbn', 'title', 'author', 'publication_yr', 'publisher']
