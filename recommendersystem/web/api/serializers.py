805d095a801d7580bd7dba1c827dcec7fabba7f4
Uriel Mandujano <uriel.mandujano14@gmail.com>
2018-10-04 22:14:41 -0500

    Removed lookup_fields options b/c we're using the default pk for lookup_field

--- recommendersystem/web/api/serializers.py
+++ recommendersystem/web/api/serializers.py
@@ -27,7 +27,6 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Book
-        lookup_field = ("title", "author")
         fields = ("id", "title", "author", "publication_yr", "publisher",
                   "isbn", "image_url_s", "image_url_m", "image_url_l")
 
@@ -67,7 +66,6 @@ class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        lookup_field = "id"
 
         # Hyper linked fields read only by default
         fields = ("id", "user_id", "isbn",  "rating",
