4f90500265dca4e8bc4c4d523650cb0d32361bca
Uriel Mandujano <uriel.mandujano14@gmail.com>
2018-10-14 23:08:57 -0500

    Pulls secret key out of project into installer

--- recommendersystem/web/recommender/settings.py
+++ recommendersystem/web/recommender/settings.py
@@ -15,12 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0#%t)jc$_@9^n$z!lxng%3g&ecahj1h8^$0p%qrh8_(j%tfua5'
+SECRET_KEY = os.getenv('SECRET_KEY', 'recommender_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
