4f3cbe59c7cc80056d33ba40b4cc5704da80b6d8
Uriel Mandujano <uriel.mandujano14@gmail.com>
2018-11-03 16:07:58 -0500

    Destroys test database automatically even after corruption

---
+++ installer/roles/image_build/files/start.sh
@@ -0,0 +1,22 @@
+#!/bin/ash
+
+# Apply any pending migrations
+python manage.py makemigrations
+python manage.py migrate --no-input
+
+# Preload database
+python manage.py loadcsv -users_csv $USERS \
+  -books_csv $BOOKS -ratings_csv $RATINGS
+
+# Run tests
+#TODO remove when finished
+python manage.py test --noinput 
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn recommender.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3
