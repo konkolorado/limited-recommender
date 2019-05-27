fbf31ed9f9a088ea5116c16eb31a9360cc73bf02
Uriel Mandujano <uriel.mandujano14@gmail.com>
2019-05-26 23:32:35 -0500

    Refactors start file for each container's responsibilities

--- installer/roles/image_build/files/start_task.sh
+++ installer/roles/image_build/files/start_task.sh
@@ -1,3 +1,20 @@
 #!/bin/ash
 
+# Wait for database to start accepting connections
+while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+  sleep 1
+done
+
+# Apply any pending migrations
+python manage.py makemigrations
+python manage.py migrate --no-input
+
+# Preload database
+python manage.py loadcsv -users_csv $USERS \
+  -books_csv $BOOKS -ratings_csv $RATINGS
+
+# Run tests
+#TODO remove when finished
+python manage.py test --noinput
+
 ./manage.py init_recommendations
--- installer/roles/image_build/files/start_web.sh
+++ installer/roles/image_build/files/start_web.sh
@@ -5,18 +5,6 @@ while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
   sleep 1
 done
 
-# Apply any pending migrations
-python manage.py makemigrations
-python manage.py migrate --no-input
-
-# Preload database
-python manage.py loadcsv -users_csv $USERS \
-  -books_csv $BOOKS -ratings_csv $RATINGS
-
-# Run tests
-#TODO remove when finished
-python manage.py test --noinput
-
 # Collect static files
 python manage.py collectstatic --no-input --link
 
