e7ff9e9c14be1dde658baa3957a06d18098a90b6
Uriel Mandujano <uriel.mandujano14@gmail.com>
2019-07-20 23:22:41 -0500

    Adds broker info to settings and deployment

--- recommendersystem/recommender/settings.py
+++ recommendersystem/recommender/settings.py
@@ -100,11 +100,17 @@ DATABASES = {
     }
 }
 
+BROKER_URL = 'amqp://{}:{}@{}:{}/{}'.format(
+    os.getenv("RABBITMQ_USER"),
+    os.getenv("RABBITMQ_PASSWORD"),
+    os.getenv("RABBITMQ_HOST"),
+    os.getenv("RABBITMQ_PORT"),
+    os.getenv("RABBITMQ_VHOST")
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS=[
     {
         'NAME':
             'django.contrib.auth.password_validation.' +
@@ -128,18 +134,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE='en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE='UTC'
 
-USE_I18N = True
+USE_I18N=True
 
-USE_L10N = True
+USE_L10N=True
 
-USE_TZ = True
+USE_TZ=True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL='/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
