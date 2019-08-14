dd479d3f586b4b13c46aa053fe56f0b1104090e4
Uriel Mandujano <uriel.mandujano14@gmail.com>
2019-08-13 21:38:43 -0500

    Adds django management utility to make django code reload easier

---
+++ recommendersystem/recommender/management/commands/django_reload.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+
+import os
+import signal
+import subprocess
+
+
+class Command(BaseCommand):
+    help = 'Gracefully reloads the gunicorn workers by sending the process a ' \
+        'HUP signal "kill -HUP <pid>"'
+
+    def handle(self, *args, **options):
+        """
+        This relies entirely on the gunicorn process being started with the
+        --pid flag, which creates a file containing the gunicorn pid.  It is
+        also assumed that this file is at /gunicorn.pid
+        """
+        try:
+            with open("/gunicorn.pid") as f:
+                pid = int(f.read().strip())
+                os.kill(pid, signal.SIGHUP)
+        except FileNotFoundError:  # Not running a gunicorn process
+            subprocess.call(["supervisorctl", "-c", "/supervisor_task.conf",
+                             "restart", "all"])
