37eb8d1c4d23b364fc7daf12eb6af92a2ec88a91
Uriel Mandujano <uriel.mandujano14@gmail.com>
2019-09-18 20:33:17 -0500

    Renamed rated field in model to source, renamed vars in init_recs

--- recommendersystem/recommender/management/commands/init_recs.py
+++ recommendersystem/recommender/management/commands/init_recs.py
@@ -31,18 +31,18 @@ class Command(BaseCommand):
         """
         return list([0]) * self.n_users
 
-    def compute_similarities(self, item, co_rated_items, precision=14):
+    def compute_similarities(self, source, item_to_item, precision=14):
         """
         Computes cosine similarity scores between item and all other items
         in co_rated_items dictionary
         """
         similarities = dict()
-        for co_rated in co_rated_items:
-            if co_rated == item:
+        for other_item in item_to_item:
+            if other_item == source:
                 continue
-            score = self.calc_cosine_similarity(co_rated_items[item],
-                                                co_rated_items[co_rated])
-            similarities[co_rated] = round(score, precision)
+            score = self.calc_cosine_similarity(item_to_item[source],
+                                                item_to_item[other_item])
+            similarities[other_item] = round(score, precision)
         return similarities
 
     def commit_similarities(self, item, similarities):
@@ -53,7 +53,7 @@ class Command(BaseCommand):
         """
         for similar_item_isbn in similarities:
             _, new = Similarity.objects.get_or_create(
-                rated=Book.objects.get(isbn=item),
+                source=Book.objects.get(isbn=item),
                 target=Book.objects.get(isbn=similar_item_isbn),
                 similarity_score=similarities[similar_item_isbn])
             if new:
@@ -62,23 +62,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # For each item in catalog
         for book in Book.objects.all():
-            co_rated_items = defaultdict(self.ddict_callable)
+            item_to_item = defaultdict(self.ddict_callable)
 
             # For each customer c who purchased item
             for rating in Rating.objects.filter(isbn__id=book.id):
                 # Record c rated book and other_rated_book, part 1
-                co_rated_items[book.isbn][rating.user_id.id-1] = 1
+                item_to_item[book.isbn][rating.user_id.id-1] = 1
 
                 # For every other item purchased by customer c
-                for co_rated_book in Rating.objects.filter(
+                for rated_book in Rating.objects.filter(
                         user_id__id=rating.user_id.id).exclude(
                         isbn__id=book.id):
 
                     # Record c rated book and other_rated_book, part 2
-                    co_rated_items[co_rated_book.isbn.isbn][rating.user_id.id-1] = 1
+                    item_to_item[rated_book.isbn.isbn][rating.user_id.id-1] = 1
 
             # For each item bought along with item, compute similarity
-            sims = self.compute_similarities(book.isbn, co_rated_items)
+            sims = self.compute_similarities(book.isbn, item_to_item)
 
             # Store item to item similarities
             self.commit_similarities(book.isbn, sims)
--- recommendersystem/recommender/models.py
+++ recommendersystem/recommender/models.py
@@ -3,8 +3,8 @@ from bookcrossing.models import Book
 
 
 class Similarity(models.Model):
-    rated = models.ForeignKey(Book, on_delete=models.PROTECT,
-                              related_name='predictive_item')
+    source = models.ForeignKey(Book, on_delete=models.PROTECT,
+                               related_name='predictive_item')
     target = models.ForeignKey(Book, on_delete=models.PROTECT,
                                related_name='recommended_item')
     similarity_score = models.DecimalField(max_digits=15, decimal_places=14)
